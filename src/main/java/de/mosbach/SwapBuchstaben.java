8bddce374ee71a098ef12d8cf25f14ee42701ff0
Stephan Mosbach <stephan.mosbach@hs.furtwangen.de>
2016-05-10 15:15:09 +0200

    Nachricht

---
+++ src/main/java/de/mosbach/SwapBuchstaben.java
@@ -0,0 +1,15 @@
+package de.mosbach;
+import java.util.Scanner;
+
+public class SwapBuchstaben{
+	public static void main(String args[])
+	{
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Eingabe: ");
+	String eingabe = sc.next();
+	System.out.println("Das wirde eingegeben: " + eingabe);
+	String gross = eingabe.toUpperCase();
+	System.out.println("Umgewandelt: " + gross);
+	}
+	
+}
